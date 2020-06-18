a5485bbf4c19969ee9e95bd00fa5c149d8474a55
Henry Wang <henry.h.wang93@gmail.com>
2020-06-18 16:21:10 -0400

    add GET express route and functions for seeding banking and users tables

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../database/index.js');
+const connection = require('../database/index.js');
+const data = require('./data.js');
 
 const app = express();
 const PORT = 3000;
-
+// console.log('connection: ', connection.query);
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../src'));
 
@@ -12,32 +13,48 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-// POST route
-app.post('/users', function (req, res) {
-  let newUser = new db.models.User({
-    name: req.body.name,
-    budget: req.body.budget,
-  });
-  newUser
-    .save()
-    .then(() => {
-      res.sendStatus(201);
-    })
-    .catch((err) => {
-      console.log('err in app.post: ', err);
-      res.sendStatus(404);
+// console.log('data: ', data);
+
+/* ---------------------- SEEDING BANKING AND USERS TABLES ---------------------- */
+const seedBankingTable = () => {
+  for (let i = 0; i < data.bankingSeed.length; i++) {
+    let queryStr = `INSERT INTO banking (i_date, i_description, amount, i_transaction, category, account_name, user_name) VALUES ("${data.bankingSeed[i].i_date}", "${data.bankingSeed[i].i_description}", "${data.bankingSeed[i].amount}", "${data.bankingSeed[i].i_transaction}", "${data.bankingSeed[i].category}", "${data.bankingSeed[i].account_name}", "${data.bankingSeed[i].user_name}")`;
+    connection.query(queryStr, function (err, results) {
+      if (err) {
+        console.log('err in seedBankingTable: ', err);
+      }
     });
-});
+  }
+};
+
+// UNCOMMENT BELOW TO POPULATE BANKING TABLE
+// seedBankingTable();
+
+const seedUserTable = () => {
+  for (let i = 0; i < data.userSeed.length; i++) {
+    let queryStr = `INSERT INTO users (user_name, monthly_budget) VALUES ("${data.userSeed[i].user_name}", "${data.userSeed[i].monthly_budget}")`;
+    connection.query(queryStr, function (err, results) {
+      if (err) {
+        console.log('err in seedUserTable: ', err);
+      }
+    });
+  }
+};
+
+// UNCOMMENT BELOW TO POPULATE USER TABLE
+// seedUserTable();
+
+/* ---------------------- EXPRESS ROUTING ---------------------- */
 
 // GET route
 app.get('/banking', function (req, res) {
-  db.models.Transactions.find({})
-    .exec()
-    .then((data) => {
-      res.send(200).json(data);
-    })
-    .catch((err) => {
+  const queryStr = 'SELECT * FROM banking ORDER BY id ASC';
+  connection.query(queryStr, (err, data) => {
+    if (err) {
       console.log('err in app.get: ', err);
       res.sendStatus(404);
-    });
+    } else {
+      res.status(200).json(data);
+    }
+  });
 });
