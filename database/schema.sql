16ffe429c6cc0a5cfdb1a989a2d91b36fb75daae
Henry Wang <henry.h.wang93@gmail.com>
2020-06-18 16:09:32 -0400

    change the date field format to text instead of date

--- database/schema.sql
+++ database/schema.sql
@@ -10,12 +10,11 @@ CREATE TABLE users (
 
 CREATE TABLE banking (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  i_date date, 
-  i_description TEXT, 
+  i_date TEXT,
+  i_description TEXT,
   amount INT,
-  i_transaction TEXT, 
-  i_type TEXT,
-  category TEXT, 
+  i_transaction TEXT,
+  category TEXT,
   account_name TEXT,
   user_name VARCHAR(255) NOT NULL
 );
