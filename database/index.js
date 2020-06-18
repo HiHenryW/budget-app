4137a626c20166ff87d4fa376d6e8205d5f1129e
AJ Benjumea <aj@AJs-Mac-mini.local>
2020-06-18 12:12:50 -0400

    Add sql schema

--- database/index.js
+++ database/index.js
@@ -1,2 +1,25 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+const config = require('../config.js')
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : config.sql,
+  database : 'budget'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+module.exports = connection; 
+
+
+
+
+
