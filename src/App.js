254107af8878de100035f799fcdde5a8ea542703
AJ Benjumea <aj@AJs-Mac-mini.local>
2020-06-18 20:33:14 -0400

    Add react-bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import TotalSpend from './components/totalSpend.js';
 import Income from './components/income.js';
 import Transactions from './components/transactions.js';
 import UserForm from './components/userForm.js';
+import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,13 +24,23 @@ class App extends React.Component {
   }
   render() {
     return (
-    <div className="main">
-      <h1>Where My Money At</h1>
+      <div>
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" sticky="top">
+        <Navbar.Brand href="#home">Budget App</Navbar.Brand>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#budget">Budget</Nav.Link>
+            <Nav.Link href="#transactions">Transactions</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="main">
       <MaxBudget user={this.state.user}/>
       <TotalSpend/>
       <Income/>
       <Transactions/>
       <UserForm/>
+    </div>
     </div>
     )
   }
