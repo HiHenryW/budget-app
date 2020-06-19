1f91f0720af410320e0cb787857fc2f4b05b6400
Henry Wang <henry.h.wang93@gmail.com>
2020-06-18 21:25:35 -0400

    add POST route handler in server

--- server/index.js
+++ server/index.js
@@ -58,3 +58,16 @@ app.get('/banking', function (req, res) {
     }
   });
 });
+
+// POST route
+app.post('/users', function (req, res) {
+  let queryStr = `INSERT INTO users (user_name, monthly_budget) VALUES ("${req.body.name}", "${req.body.budget}")`;
+  connection.query(queryStr, function (err, results) {
+    if (err) {
+      console.log('err in app.post: ', err);
+      res.sendStatus(404);
+    } else {
+      res.status(201);
+    }
+  });
+});
