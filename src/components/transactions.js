0fb233b9b7d55c5e4261e717abd08ce894bc751c
Henry Wang <henry.h.wang93@gmail.com>
2020-06-19 04:02:40 -0400

    add show more/less buttons to expand/minimize transactions

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -9,13 +9,31 @@ class Transactions extends React.Component {
     this.state = {
       transactions: [],
       recentTransactions: [],
+      dataRows: 5,
     };
 
+    this.updateRecentTransactions = this.updateRecentTransactions.bind(this);
     this.retrieveTransactions = this.retrieveTransactions.bind(this);
+    this.handleExpandClick = this.handleExpandClick.bind(this);
+    this.handleMinimizeClick = this.handleMinimizeClick.bind(this);
   }
 
   componentDidMount() {
     this.retrieveTransactions();
+  }
+
+  updateRecentTransactions() {
+    let subList = [];
+    for (let i = 0; i < this.state.dataRows; i++) {
+      let transaction = {};
+      transaction.index = i + 1;
+      transaction.date = this.state.transactions[i].i_date;
+      transaction.description = this.state.transactions[i].i_description;
+      transaction.amount = this.state.transactions[i].amount;
+      transaction.category = this.state.transactions[i].category;
+      subList.push(transaction);
+    }
+    this.setState({ recentTransactions: subList });
   }
 
   retrieveTransactions() {
@@ -28,24 +46,25 @@ class Transactions extends React.Component {
         // console.log('state: ', this.state.transactions[0])
       })
       .then(() => {
-        let smallerList = [];
-        for (let i = 0; i < 5; i++) {
-          let transaction = {};
-          transaction.index = i + 1;
-          transaction.date = this.state.transactions[i].i_date;
-          transaction.description = this.state.transactions[i].i_description;
-          transaction.amount = this.state.transactions[i].amount;
-          transaction.category = this.state.transactions[i].category;
-          smallerList.push(transaction);
-        }
-        this.setState({ recentTransactions: smallerList });
+        this.updateRecentTransactions();
       })
-      .then(() => {
-        console.log(this.state.recentTransactions);
-      })
+      // .then(() => {
+      //   console.log(this.state.recentTransactions);
+      // })
       .catch((err) => {
         console.log('err in retrieveTransactions: ', err);
       });
+  }
+
+  handleExpandClick(event, callback) {
+    event.preventDefault();
+    let increment = this.state.dataRows + 25;
+    this.setState({ dataRows: increment }, this.updateRecentTransactions);
+  }
+
+  handleMinimizeClick(event) {
+    event.preventDefault();
+    this.setState({ dataRows: 5 }, this.updateRecentTransactions);
   }
 
   render() {
@@ -79,6 +98,21 @@ class Transactions extends React.Component {
               </tbody>
             </table>
           </table>
+          <button
+            type="button"
+            class="btn btn-outline-primary"
+            onClick={this.handleExpandClick}
+          >
+            Show more
+          </button>
+          <span> </span>
+          <button
+            type="button"
+            class="btn btn-outline-secondary"
+            onClick={this.handleMinimizeClick}
+          >
+            Show less
+          </button>
         </div>
       </div>
     );
