c24971aa2bd0ddca37caf74957f320a1106c264f
Henry Wang <henry.h.wang93@gmail.com>
2020-06-18 23:08:43 -0400

    Change i_date format back to DATE

--- database/schema.sql
+++ database/schema.sql
@@ -2,19 +2,25 @@ CREATE DATABASE budget;
 
 USE budget;
 
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_name VARCHAR(255) NOT NULL,
+CREATE TABLE users
+(
+  id INT
+  AUTO_INCREMENT PRIMARY KEY,
+  user_name VARCHAR
+  (255) NOT NULL,
   monthly_budget INT
 );
 
-CREATE TABLE banking (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  i_date TEXT,
-  i_description TEXT,
-  amount INT,
-  i_transaction TEXT,
-  category TEXT,
-  account_name TEXT,
-  user_name VARCHAR(255) NOT NULL
+  CREATE TABLE banking
+  (
+    id INT
+    AUTO_INCREMENT PRIMARY KEY,
+    i_date DATE,
+    i_description TEXT,
+    amount INT,
+    i_transaction TEXT,
+    category TEXT,
+    account_name TEXT,
+    user_name VARCHAR
+    (255) NOT NULL
 );
