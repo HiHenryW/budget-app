f324f3d1234b39b5bccf34fe7bb08cea364271a2
Henry Wang <henry.h.wang93@gmail.com>
2020-06-19 01:46:19 -0400

    add retrieveTransactions method for transactions.js

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -1,13 +1,80 @@
 // List of last ~5 transactions  (w/ view all link)
 import React from 'react';
+import axios from 'axios';
 
-let Transactions = () => {
-  return (
-    <div>
-      <h2>Last 5 Transactions</h2>
-      <p>see all</p>
-    </div>
-  )
+class Transactions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: [],
+    };
+
+    this.retrieveRecentTransactions = this.retrieveRecentTransactions.bind(
+      this
+    );
+  }
+
+  componentDidMount() {
+    this.retrieveTransactions();
+  }
+
+  retrieveTransactions(params) {
+    axios
+      .get('http://localhost:3000/banking')
+      .then((res) => {
+        this.setState({
+          transactions: res.data,
+        });
+        // console.log(res)
+      })
+      .catch((err) => {
+        console.log('err in retrieveRecentTransactions: ', err);
+      });
+      console.log(this.state.transactions)
+  }
+
+  render() {
+    return (
+      <div class="row justify-content-center">
+        <div class="col-auto">
+          <h2>Recent Transactions</h2>
+          <table class="table table-responsive">
+            <table class="table">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Amount ($)</th>
+                  <th scope="col">Category</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Transactions;
